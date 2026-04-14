0512be00869228f984a488652658fd87e3a7b44d
RATest35 <ryant.student@gmail.com>
2026-04-14 12:39:00 -0500

    generate_graph_model.py: added a seed value for debugging and consistency of data. Added a 'bytes_per_sec' column to store in the edges. Using bell curve for random to keep values lower.
    composite_score.py: removing the file paths for so users can set their own.
    Added a normalization function to allow for setting ranges for each attribute.
    using bytes_per_sec for pagerank weight.
    
    Composite Score value uses normalized values to compute the score.

--- examples/ip-address/generate_graph_model.py
+++ examples/ip-address/generate_graph_model.py
@@ -3,17 +3,27 @@ import networkx as nx
 import pandas as pd
 
 DATA_PATH = r""
+SEED = 74
+random.seed(SEED)
+
+def bell_curve_probability(mean=0.1, std_dev=0.04):
+    return max(0.0, min(1.0, random.gauss(mean, std_dev)))
 
 df = pd.read_csv(DATA_PATH,
-                 usecols=["Source.IP","Destination.IP", "Total.Length.of.Fwd.Packets"])
+                 usecols=["Source.IP","Destination.IP", "Total.Length.of.Fwd.Packets", "Flow.Bytes.s"])
 
 # Csv with source and destination IPs and the sum of the bytes from source to destination stored as flow
-df_sum_flow = df.groupby(["Source.IP", "Destination.IP"], as_index=False).agg(flow=("Total.Length.of.Fwd.Packets", "sum"))
+df_sum_flow = df.groupby(["Source.IP", "Destination.IP"], as_index=False).agg(flow=("Total.Length.of.Fwd.Packets", "sum"),
+                                                                               bytes_per_sec=("Flow.Bytes.s", "median"))
 
 G = nx.DiGraph()
-# Zip changes the rows from separate columns into a tuple allowing for edges to be added
-G.add_weighted_edges_from(zip(df_sum_flow["Source.IP"], df_sum_flow["Destination.IP"], df_sum_flow["flow"]),
-                          weight="flow")
+for _, row in df_sum_flow.iterrows():
+    G.add_edge(
+        row["Source.IP"],
+        row["Destination.IP"],
+        flow=row["flow"],
+        bytes_per_sec=row["bytes_per_sec"]
+    )
 
 # Find the largest connected component
 cc = max(nx.weakly_connected_components(G), key=len)
@@ -21,7 +31,7 @@ U = G.subgraph(cc).copy()
 
 
 # Compute flow-loss (importance value) for each node, using in & out edges
-for n in U.nodes():
+for n in sorted(U.nodes()):
 
     incoming_flow = 0.0
     # U.in_edges returns a list of tuples (u, v, data), where data is a list of the attributes stored
@@ -43,9 +53,9 @@ for n in U.nodes():
     U.nodes[n]["flow_loss"] = incoming_flow + outgoing_flow
 
 for n in U.nodes():
-    U.nodes[n]["random_probability"] = random.random()
+    U.nodes[n]["random_probability"] = bell_curve_probability()
 
 
 # Write out H as a graphml file
-nx.write_graphml(U, f"ip_graph_{len(U.nodes())}_nodes_{len(U.edges())}_edges_with_flow.graphml")
-print("Nodes:", U.number_of_nodes(), "Edges:", U.number_of_edges())
+nx.write_graphml(U, f"ip_graph_{len(U.nodes())}_nodes_{len(U.edges())}_edges_seed{SEED}.graphml")
+print("Nodes:", U.number_of_nodes(), "Edges:", U.number_of_edges(), "Seed:", SEED)
